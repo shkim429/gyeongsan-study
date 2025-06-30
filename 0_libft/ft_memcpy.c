5adcefc905080d2a30323197407986ddc3dc4522
Sohui Kim <sohuikim@c3r11s1.42gyeongsan.kr>
2025-06-30 21:11:51 +0900

    libft_add3

--- 0_libft/ft_memcpy.c
+++ 0_libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: sohuikim <sohuikim@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 09:38:07 by sohuikim          #+#    #+#             */
-/*   Updated: 2025/06/29 22:33:12 by sohuikim         ###   ########.fr       */
+/*   Updated: 2025/06/30 03:49:30 by sohuikim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,30 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char	*d;
-	unsigned char	*s; 
+	unsigned char	*s;
+	unsigned char	*tmp;
 
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
+	tmp = d;
 	while (n--)
 	{
 		*d++ = *s++;
 	}
-	return (d);
+	return (tmp);
 }
 
 int main()
 {
-	char src1[32] = "Source memory";
-	char dest1[32] = "Destination memory";
+	char src1[10] = "1234567";
+	char dest1[10] = "890";
 
-	// char src2[15] = "12345678";
-	// char dest2[15] = "99999999";
+	char src2[10] = "123";
+	char dest2[10] = "456";
+
 
 	printf("src1 = %s, dest1 = %s\n", src1, dest1);
-	printf("src1 = %s, dest1 = %s", src1, (char *)memcpy(dest1, src1, sizeof(src1)));
-	// printf("src2 = %s, dest2 = %s\n", src2, dest2);
-	// printf("src2 = %s, dest2 = %s", src2, ft_memcpy(dest2, src2, sizeof(src2)));
+	printf("src1 = %s, dest1 = %s\n\n", src1, (char *)memcpy(src1+1, src1, 5));
+	printf("src2 = %s, dest2 = %s\n", src2, dest2);
+	printf("src2 = %s, dest2 = %s", src2, (char *)ft_memcpy(dest2, src2, 5));
 }
