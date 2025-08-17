77286fd41f0851b064a40c28b4f8b779324ad53b
Sohui Kim <sohuikim@c1r20s1.42gyeongsan.kr>
2025-08-18 08:53:06 +0900

    ft_get_next_line: initial commit

---
+++ m1/ft_get_next_line/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sohuikim <sohuikim@student.42gyeongsan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/18 01:25:17 by sohuikim          #+#    #+#             */
+/*   Updated: 2025/08/18 08:33:36 by sohuikim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+
+#endif
