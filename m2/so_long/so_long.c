f973c5faeeeec8b2d0db9d5bb71a6e0f805c803e
sohuikim <sohuikim@student.42gyeongsan.kr>
2026-01-19 16:11:10 +0900

    mlx: render XPM image coverted from PNG to window following 42Docs tutorial

--- m2/so_long/so_long.c
+++ m2/so_long/so_long.c
@@ -24,14 +24,20 @@ int	main(void)
 	t_vars	vars;
 	t_data	img;
 	char	*relative_path;
+	int		img_w;
+	int		img_h;
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 500, 500, "Hello world");
+	relative_path = "test_img.xpm";
+	vars.win = mlx_new_window(vars.mlx, 2000, 2000, "Hello world");
+	img.img = mlx_xpm_file_to_image(vars.mlx, relative_path, &img_w, &img_h);
+	if (img.img == NULL)
+		return (NULL);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
 
-	img.img = mlx_new_image(vars.mlx, 500, 500);
+	// img.img = mlx_new_image(vars.mlx, 500, 500);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	//my_mlx_pixel_put(&img, 100, 100, 0x00FF0000);
-	//mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
 	// mlx_key_hook(vars.win, key_hook, &vars);
 	mlx_hook(vars.win, 2, 1L<<0, handle_exit_key, &vars);
 	mlx_loop(vars.mlx);
