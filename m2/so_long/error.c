e4dc515eb06e6662bed4d7208bcea80354f15ff0
Sohui Kim <sohuikim@c1r23s8.42gyeongsan.kr>
2026-02-09 15:19:55 +0900

    so_long: fix error message prefix to Error

--- m2/so_long/error.c
+++ m2/so_long/error.c
@@ -6,7 +6,7 @@
 /*   By: sohuikim <sohuikim@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 00:09:20 by sohuikim          #+#    #+#             */
-/*   Updated: 2026/02/08 15:25:39 by sohuikim         ###   ########.fr       */
+/*   Updated: 2026/02/09 15:17:21 by sohuikim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	print_error(char *error_title, char *error_msg)
 {
-	write(STDERR_FILENO, "error\n", 6);
+	write(STDERR_FILENO, "Error\n", 6);
 	write(STDERR_FILENO, error_title, ft_strlen(error_title));
 	write(STDERR_FILENO, ": ", 2);
 	write(STDERR_FILENO, error_msg, ft_strlen(error_msg));
